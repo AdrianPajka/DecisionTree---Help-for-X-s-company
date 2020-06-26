9b6710b1dc1c69d206f75166ec6e52c564632af1
AdrianPajka <51638840+AdrianPajka@users.noreply.github.com>
2020-06-26 16:13:16 +0200

    typo changing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from decisionTree import tree, classify, Candidate
 
-print("Hello, you are using interview helper for X's company, fulfill data to see if candidate is good enough to work here/n")
+print("Hello, you are using interview helper for X's company, fulfill data to see if candidate is good enough to work here")
 level = input("Level of candidate is [Junior, Mid, Senior]:   ")
 lang = input("Candidate's main programming language is [C#, JavaScript, Python, Ruby, Java, C++]: ")
 social_media = input("Is Candidate active in SocialMedia? [True, False]: ")
